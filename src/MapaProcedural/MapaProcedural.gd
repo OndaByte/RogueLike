187b1ff65d0cb4e1899ee94a3e0ecc71094158b8
Fran <rosasfran.97@gmail.com>
2024-01-05 15:36:07 -0300

    movimiento en el aire listo

--- src/MapaProcedural/MapaProcedural.gd
+++ src/MapaProcedural/MapaProcedural.gd
@@ -31,7 +31,6 @@ func generar_habitaciones():
 	var y_aux: int = 0
 	var x_aux: int = 0
 	while(can_habitaciones>i):
-		
 		while (grid_map.get_cell_item(Vector3(x_aux,y_aux,z_aux)) != grid_map.INVALID_CELL_ITEM):
 			z_aux = randi() % z_actual
 			y_aux = randi() % y_actual
@@ -41,26 +40,18 @@ func generar_habitaciones():
 
 func visualize_border():
 	grid_map.clear()
-	for i in range(0,z_actual):
+	for i in range(0,z_actual+1):
 		grid_map.set_cell_item(Vector3i(0,0,i),1)
 		grid_map.set_cell_item(Vector3i(x_actual,0,i),1)
 		grid_map.set_cell_item(Vector3i(0,y_actual,i),1)
 		grid_map.set_cell_item(Vector3i(x_actual,y_actual,i),1)
-	
-	for i in range(0,x_actual):
-		grid_map.set_cell_item(Vector3i(i,0,0),1)
-		grid_map.set_cell_item(Vector3i(i,0,z_actual),1)
-		grid_map.set_cell_item(Vector3i(i,y_actual,0),1)
-		grid_map.set_cell_item(Vector3i(i,y_actual,z_actual),1)
-		
-	for i in range(0,y_actual):
+		for t in range(1,x_actual):
+			grid_map.set_cell_item(Vector3i(t,0,i),1)
+			grid_map.set_cell_item(Vector3i(t,y_actual,0),1)
+			grid_map.set_cell_item(Vector3i(t,y_actual,z_actual),1)
+	for i in range(1,y_actual):
 		grid_map.set_cell_item(Vector3i(0,i,0),1)
 		grid_map.set_cell_item(Vector3i(0,i,z_actual),1)
 		grid_map.set_cell_item(Vector3i(x_actual,i,0),1)
 		grid_map.set_cell_item(Vector3i(x_actual,i,z_actual),1)
-		
 	generar_habitaciones()
-	
-func generate():
-	print("generating ...")
-	
