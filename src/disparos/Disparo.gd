c763eef08e05b37e258783a3a5af985ac553c7fe
Fran <rosasfran.97@gmail.com>
2024-04-14 00:56:57 -0300

    Boludeces

---
+++ src/disparos/Disparo.gd
@@ -0,0 +1,11 @@
+extends Node3D
+class_name Disparo
+
+@export var daño: int = 10
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
